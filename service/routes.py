89a97cfe806342cb951d0949dcfbcd79c9b67d88
Bill Robinson <billrob7@gmail.com>
2025-05-05 21:12:06 +0000

    UI Fix

--- service/routes.py
+++ service/routes.py
@@ -32,16 +32,8 @@ from service.common import status  # HTTP Status Codes
 ######################################################################
 @app.route("/")
 def index():
-    """Root URL response"""
-    app.logger.info("Request for Root URL")
-    return (
-        jsonify(
-            name="Product Demo REST API Service",
-            version="1.0",
-            paths=url_for("list_products", _external=True),
-        ),
-        status.HTTP_200_OK,
-    )
+    """Base URL for our service"""
+    return app.send_static_file("index.html")
 
 
 ######################################################################
